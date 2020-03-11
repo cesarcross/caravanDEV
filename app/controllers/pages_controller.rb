6d5823af56a125fe10d2c38f191be03bacb9dd4e
Vinicius Alvim <vini0191@gmail.com>
2020-03-11 13:29:26 -0300

    modify pages_controller to work with desktop and mobile pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,19 +1,25 @@
+# frozen_string_literal: true
+
 class PagesController < ApplicationController
   # skip_before_action :authenticate_user!, only: [:home]
   skip_before_action :authenticate_user!, only: %i[home new create]
 
   def home
+    respond_to do |format|
+      format.json
+      format.html # /app/views/home/home.html.erb
+      format.html.mobile # /app/views/home/home.html+mobile.erb
+    end
     @page = Page.new
   end
 
-  def new
-  end
+  def new; end
 
   def create
     @page = Page.new(page_params)
     if @page.deliver
       # render json: { message: "Email sent successfully" }
-      flash[:notice] = "Mensagem enviada com sucesso! Responderemos em breve."
+      flash[:notice] = 'Mensagem enviada com sucesso! Responderemos em breve.'
       redirect_to root_path
     else
       # render json: @page.errors
@@ -22,6 +28,14 @@ class PagesController < ApplicationController
   end
 
   private
+
+  # def determine_variant
+  #   variant = nil
+  #   # some code to determine the variant(s) to use
+  #   variant = :mobile if session[:use_mobile]
+
+  #   variant
+  # end
 
   def page_params
     params.require(:page).permit(:name, :email, :phone, :message)
