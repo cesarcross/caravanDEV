ac1488330c0e8c53ed3d06887035fb720554d6b4
Vinicius Alvim <vini0191@gmail.com>
2020-03-12 16:37:19 -0300

    Change smtp address

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,7 +91,8 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default charset: 'utf-8'
   config.action_mailer.smtp_settings = {
-    address: 'smtp.codecaravan.com.br',
+    # address: 'smtp.codecaravan.com.br',
+    address: 'smtp.kinghost.net',
     port: 465,
     domain: 'codecaravan.com.br',
     user_name: 'contato@codecaravan.com.br',
