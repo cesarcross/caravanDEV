a868f87db0842fd41e1407b34633ac8edc399ac3
Vinicius Alvim <vini0191@gmail.com>
2020-03-12 12:56:53 -0300

    Change contact email in both html files, comment every single line of contact model

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,17 +1,18 @@
-class Contact < MailForm::Base
-  attribute :name, validate: true
-  attribute :email, validate: true
-  attribute :message, validate: true
+# frozen_string_literal: true
+# class Contact < MailForm::Base
+#   attribute :name, validate: true
+#   attribute :email, validate: true
+#   attribute :message, validate: true
 
-  def headers
-    {
-      # this is the subject for the email generated, it can be anything you want
-      # subject: "My Contact Form",
-      subject: "Contato",
-      # to: 'your.email@yourdomain.com',
-      to: 'contato@codecaravan.com.br',
-      from: %("#{name}" <#{email}>)
-      # the from will display the name entered by the user followed by the email
-    }
-  end
-end
+#   def headers
+#     {
+#       # this is the subject for the email generated, it can be anything you want
+#       # subject: "My Contact Form",
+#       subject: "Contato",
+#       # to: 'your.email@yourdomain.com',
+#       to: 'contato@codecaravan.com.br',
+#       from: %("#{name}" <#{email}>)
+#       # the from will display the name entered by the user followed by the email
+#     }
+#   end
+# end
